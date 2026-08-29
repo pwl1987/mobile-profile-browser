da417fbdf1f5de267fed8c3cea5a9780495828ee
pwl1987 <13573975690@163.com>
2026-08-29 16:23:28 +0800

    fix: 导出 Profile 运行时领域契约

--- packages/mobile_profile_domain/lib/mobile_profile_domain.dart
+++ packages/mobile_profile_domain/lib/mobile_profile_domain.dart
@@ -1,5 +1,6 @@
 export 'src/mobile_profile.dart';
 export 'src/oppo_find_n3.dart';
 export 'src/provider_registry.dart';
-export 'src/runtime_instance.dart';
+export 'src/repositories.dart';
+export 'src/runtime_lifecycle.dart';
 export 'src/validators.dart';
