c7f7a6c4432873312422c505742d5b49cd727339
pwl1987 <13573975690@163.com>
2026-08-29 16:36:08 +0800

    feat: 导出领域持久化与内存仓储

--- packages/mobile_profile_domain/lib/mobile_profile_domain.dart
+++ packages/mobile_profile_domain/lib/mobile_profile_domain.dart
@@ -1,5 +1,7 @@
+export 'src/in_memory_repositories.dart';
 export 'src/mobile_profile.dart';
 export 'src/oppo_find_n3.dart';
+export 'src/profile_codec.dart';
 export 'src/provider_registry.dart';
 export 'src/repositories.dart';
 export 'src/runtime_lifecycle.dart';
